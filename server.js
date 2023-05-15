2c32220c819e0f8be724af69af254664bb6f8441
Augusto Fernando Klein <augusto@MacBook-Air-de-Augusto.local>
2023-05-14 21:31:58 -0300

    Creation of the project and addition of procedures

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+
+http.createServer((request, response) => {
+    response.writeHead(200, {"Content-Type": "application/json"});
+
+    if(request.url === "/produto"){
+        response.end(
+            JSON.stringify({
+                message: "Rota de produto"
+            })
+        );
+    }
+
+    if(request.url === "/usuario"){
+        response.end(
+            JSON.stringify({
+                message: "Rota de usuarios"
+            })
+        );
+    }
+
+    response.end(
+        JSON.stringify({
+            message: "Qualquer outra rota"
+        })
+    );
+})
+.listen(4001, () => console.log("Servidor está rodando na porta 4001"));
